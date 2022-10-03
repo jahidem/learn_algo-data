fe00758c0cd81ae7499bfe6a68fae06c6f4ecccc
Jahid Hossain <54525656+jahidem@users.noreply.github.com>
2022-10-03 09:48:46 +0600

    garbz

--- huh/A.cpp
+++ huh/A.cpp
@@ -1,28 +1,6 @@
-#include <bits/stdc++.h>
+#include<iostream>
 using namespace std;
-using ll = long long;
 int main()
 {
-  ios_base::sync_with_stdio(false);
-  cin.tie(NULL);
-   //freopen("inp", "r", stdin);
-  ll T = 1;  cin>>T;
-  {
-    int n,m;cin>>n>>m;
-    int ls[n][m]={};
-    int nw;
-    for(int u=0;u<n;u+=2){
-      nw=u%2;
-      for(int v=0;v<m;v+=2){
-        if(nw){
-          ls[u+1][v+1]=ls[u][v]=1;
-        }
-        else ls[u][v+1]=ls[u+1][v]=1;
-      nw++,nw%=2;}
-    }
-  for(int u=0;u<n;u++){
-      for(int v=0;v<m;v++){
-        cout<<ls[u][v]<<" ";
-    }cout<<"\n";}
-  }
+  cout<<"works";
 }
--- huh/tempCodeRunnerFile.cpp
+++ huh/tempCodeRunnerFile.cpp
@@ -1,3 +1,20 @@
-unsigned char pMap[] = {1, 1, 1, 1, 0, 1, 0, 1, 0, 1, 1, 1};
-int pOutBuffer[12];
-cout<<FindPath(0, 0, 1, 2, pMap, 4, 3, pOutBuffer, 12);
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+int main()
+{
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+   //freopen("inp", "r", stdin);
+  ll T = 1;  //cin>>T;
+  for(int tt=1;tt<=T;tt++){ 
+    int l1,l2,r1,r2;
+    cin>>l1>>r1>>l2>>r2;
+    if(l1<=l2&&l2<r1)
+      cout<<min(r1,r2)-l2;
+    else if(l2<=l1&&l1<r2)
+       cout<<min(r2,r1)-l1;
+    else cout<<0;
+
+  }
+}
